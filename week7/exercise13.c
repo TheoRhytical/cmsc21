5249c3b6ee6da346c1c752717bb996a77620c506
AnotherTheoRhysT <rhyscabonita@gmail.com>
2023-04-02 15:09:09 +0800

    Exercise 13: Just copied from sir's slides

--- week7/exercise13.c
+++ week7/exercise13.c
@@ -1,92 +1,91 @@
 #include <stdlib.h>
-#include <stdio.h>
-
 
 typedef struct LinkedListNode {
-    void* item;
+    void* value;
     struct LinkedListNode* next;
 } LinkedListNode;
 
 
 typedef struct {
-    struct LinkedListNode* head;
+    LinkedListNode* head;
     int size;
 } LinkedList;
 
 
+LinkedListNode* createNode(void* item) {
+    LinkedListNode* node = malloc(sizeof(LinkedListNode));
+    node->value = item;
+    node->next = NULL;
+    return node;
+}
+
+
+LinkedListNode* getNode(LinkedList* list, int index) {
+    int currentIndex = 0;
+    LinkedListNode* currentNode = list->head;
+
+    while(currentIndex < index) {
+        currentNode = currentNode->next;
+        currentIndex++;
+    }
+
+    return currentNode;
+}
+
+
 void* insertItem(LinkedList* list, int index, void* item) {
-	if (index > list->size || index < 0) return NULL;
-	LinkedListNode* currNode = list->head;
-	LinkedListNode* prevNode;
-	for (int i = 0; i <= list->size; i++) {
-		if (i == index) {
-			// Insert
-			LinkedListNode* newNode = (LinkedListNode*) malloc(sizeof(LinkedListNode));
-			newNode->item = item;
-			if (i == 0) {
-				newNode->next = currNode;
-				list->head = newNode;
-			} else {
-				newNode->next = currNode;
-				prevNode->next = newNode;
-			}
-			
-			list->size++;
-			return newNode->item;
-		}
-		prevNode = currNode;
-		currNode = currNode->next;
-	}
-	return NULL;
+    if (index < 0 || index > list->size) return NULL;
+
+    LinkedListNode* newNode = createNode(item);
+    if (index == 0) {
+        newNode->next = list->head;
+        list->head = newNode;
+    } else {
+        LinkedListNode* previousNode = getNode(list, index - 1);
+
+        newNode->next = previousNode->next;
+        previousNode->next = newNode;
+    }
+    list->size++;
+
+    return newNode->value;
 }
 
 
 void* getItem(LinkedList* list, int index) {
-	if (index > list->size || index < 0) return NULL;
-	LinkedListNode* currNode = list->head;
-	for (int i = 0; i < list->size; i++) {
-		if (i == index) return currNode->item;
-		currNode = currNode->next;
-	}
-	return NULL;
+    if (index < 0 || index >= list->size) return NULL;
+
+    LinkedListNode* node = getNode(list, index);
+    return node->value;
 }
 
 
 void* setItem(LinkedList* list, int index, void* item) {
-	if (index > list->size || index < 0) return NULL;
-	LinkedListNode* currNode = list->head;
-	for (int i = 0; i < list->size; i++) {
-		if (i == index) {
-			currNode->item = item;
-			return currNode->item;
-		}
-		currNode = currNode->next;
-	}
-	return NULL;
+    if (index < 0 || index >= list->size) return NULL;
+
+    LinkedListNode* node = getNode(list, index);
+    void* oldItem = node->value;
+    node->value = item;
+    return oldItem;
 }
 
 
 void* deleteItem(LinkedList* list, int index) {
-	if (index > list->size || index < 0) return NULL;
-	LinkedListNode* currNode = list->head;
-	LinkedListNode* prevNode = NULL;
-	for (int i = 0; i < list->size; i++) {
-		if (index == 0) {
-			list->head = currNode->next;
-			free(currNode);
-			list->size--;
-			return (list->head == NULL) ? NULL : list->head->item;
-		} else if (i == index) {
-			if (currNode == NULL) return NULL;
-			prevNode->next = currNode->next;
-			free(currNode);
-			list->size--;
-			return prevNode->item;
-		}
-		prevNode = currNode;
-		currNode = currNode->next;
-		
-	}
-	return NULL;
-}
+    if (index < 0 || index > list->size) return NULL;
 
+    LinkedListNode* toRemove;
+    if (index == 0) {
+        toRemove = list->head;
+        list->head = list->head->next;
+    } else {
+        LinkedListNode* previous = getNode(list, index - 1);
+        toRemove = previous->next;
+        previous->next = toRemove->next;
+    }
+
+    void* oldItem = toRemove->value;
+    free(toRemove);
+    list->size--;
+
+    return oldItem;
+}
